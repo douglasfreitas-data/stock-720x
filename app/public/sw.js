27308439ec21791f4b3f0786cfd8a2d7faf79bca
douglasfreitas-data <douglas.freitas.data@gmail.com>
2026-03-14 20:03:46 -0300

    fix(push): add skipWaiting to service worker to force updates

--- app/public/sw.js
+++ app/public/sw.js
@@ -1,3 +1,11 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('push', function (event) {
     const title = '⚠️ Alerta de Estoque Mínimo';
     const options = {
